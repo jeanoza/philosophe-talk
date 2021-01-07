45b57fb9cebb26011997b52c24c882be88b9186b
jeanoza <choikb916@gmail.com>
2021-01-07 19:45:36 +0100

    js-clock add to all html

---
+++ js/js-clock.js
@@ -0,0 +1,21 @@
+const clockContainer = document.querySelector(".js-clock"),
+    clockTitle = clockContainer.querySelector("span");
+
+
+
+function getTime() {
+    const date = new Date(),
+        hours = date.getHours(),
+        minutes = date.getMinutes();
+    
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
+    console.log("reloaded");
+    
+}
+
+function init() {
+    getTime();
+    setInterval(getTime, (1000 * 60));
+}
+
+init();
